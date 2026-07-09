2c1b5d967e656c960bef0d2726780db68bd2a19a
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-07-09 18:18:12 +0530

    Time: 3 ms (46.26%), Space: 9.8 MB (25.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
-        if(s.length() != t.length())
-            return false;
-
-        int freq[26] = {0};
-
-        for(char c : s)
-            freq[c - 'a']++;
-
-        for(char c : t)
-            freq[c - 'a']--;
-
-        for(int i = 0; i < 26; i++)
-        {
-            if(freq[i] != 0)
-                return false;
+        unordered_map<char,int> check;
+        for(int i = 0; i<s.length(); i++){
+            check[s[i]]++;
         }
 
+        char temp;
+        
+        for(int i = 0; i<t.length(); i++){
+            check[t[i]]--;
+
+        }
+        for(auto x : check){
+            if(x.second != 0){
+                return false;
+            }
+        }
         return true;
     }
 };
