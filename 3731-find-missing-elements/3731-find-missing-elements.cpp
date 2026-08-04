02bb59ed4ced6977f0c0f1128305657dc4a57fe9
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-08-04 13:59:39 +0530

    Time: 0 ms (100%), Space: 32.3 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -4,28 +4,26 @@ public:
         vector<int>a;
         int min = nums[0];
         int max = 0;
-        unordered_map<int,int>n;
+        bool check = false;
         for(int i = 0; i<nums.size(); i++){
-            n[nums[i]]++;
             if(nums[i]<min){
                 min = nums[i];
             }
             if(nums[i]>max){
                 max = nums[i];
             }
-        }  
-
-        for(int i = min; i<=max;i++){
-            cout<<i;
-            n[i]++;
         }
-        
-    for(auto x : n){
-        if(x.second<2){
-            a.push_back(x.first);
+        for(int i = min; i<max; i++){
+            for(int j = 0; j<nums.size(); j++){
+                if(i==nums[j]){
+                    check = true;
+                }
+            }
+            if(check==false){
+                a.push_back(i);
+            }
+            check = false;
         }
-    }
-    sort(a.begin(),a.end());
-    return a;
+        return a;
     }
 };
