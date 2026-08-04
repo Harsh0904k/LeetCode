c3684f3b94fa92c659cae5c1764e84cabfdc8cff
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-08-04 16:26:59 +0530

    Time: 2 ms (58.4%), Space: 21.4 MB (96.78%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    bool row(vector<vector<char>>& board){
+    int n = 0;
+    while(n<9){
+        for(int i = 0; i<9;i++){
+            for(int j = i+1; j<9; j++){
+                if(board[n][i]==board[n][j] && board[n][i]!='.'){
+                    cout<<"false 1 : "<<board[n][j]<<endl;
+                    cout<<n<<","<<j<<endl;      
+                    return false;
+                }
+            }
+        }
+        n++;
+    }
+    return true;
+    }
+
+    bool column(vector<vector<char>>& board){
+    int n = 0;
+    while(n<9){
+        for(int i = 0; i<9;i++){
+            for(int j = i+1; j<9; j++){
+                if(board[i][n]==board[j][n] && board[i][n]!='.'){
+                    cout<<"false 2 : "<<i<<","<<n<<endl;
+                    cout<<j<<","<<n<<endl;      
+                    return false;
+                }
+            }
+        }
+        n++;
+    }
+    return true;
+    }
+
+
+    bool box(vector<vector<char>>& board) {
+
+    // Starting position of each 3x3 box
+    for (int row = 0; row < 9; row += 3) {
+        for (int col = 0; col < 9; col += 3) {
+
+            // Compare every pair inside one box
+            for (int i = 0; i < 9; i++) {
+                for (int j = i + 1; j < 9; j++) {
+                    
+                    int r1 = row + i / 3;
+                    int c1 = col + i % 3;
+
+                    int r2 = row + j / 3;
+                    int c2 = col + j % 3;                    
+
+                    if (board[r1][c1] == board[r2][c2] &&
+                        board[r1][c1] != '.') {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool ro = row(board);
+        bool co = column(board);
+        bool bo = box(board);
+        return ro && co && bo;
+    }
+};
