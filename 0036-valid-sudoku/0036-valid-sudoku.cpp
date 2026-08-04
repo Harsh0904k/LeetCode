4cf12ed23771dbdcaa704c9617230c41413b2493
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-08-04 16:27:34 +0530

    Time: 2 ms (58.4%), Space: 21.4 MB (99.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -6,8 +6,6 @@ public:
         for(int i = 0; i<9;i++){
             for(int j = i+1; j<9; j++){
                 if(board[n][i]==board[n][j] && board[n][i]!='.'){
-                    cout<<"false 1 : "<<board[n][j]<<endl;
-                    cout<<n<<","<<j<<endl;      
                     return false;
                 }
             }
@@ -22,9 +20,7 @@ public:
     while(n<9){
         for(int i = 0; i<9;i++){
             for(int j = i+1; j<9; j++){
-                if(board[i][n]==board[j][n] && board[i][n]!='.'){
-                    cout<<"false 2 : "<<i<<","<<n<<endl;
-                    cout<<j<<","<<n<<endl;      
+                if(board[i][n]==board[j][n] && board[i][n]!='.'){     
                     return false;
                 }
             }
@@ -36,15 +32,11 @@ public:
 
 
     bool box(vector<vector<char>>& board) {
-
-    // Starting position of each 3x3 box
     for (int row = 0; row < 9; row += 3) {
         for (int col = 0; col < 9; col += 3) {
-
-            // Compare every pair inside one box
             for (int i = 0; i < 9; i++) {
                 for (int j = i + 1; j < 9; j++) {
-                    
+
                     int r1 = row + i / 3;
                     int c1 = col + i % 3;
 
