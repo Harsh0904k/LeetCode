17abc8c066b474af6023d6c087818ffbeac0e005
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-08-05 14:45:17 +0530

    Time: 51 ms (5.38%), Space: 9.9 MB (78.02%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int i = 0; i<s.length(); i++){
+            if(s[i]==' '){
+                s.erase(s.begin()+i);
+                i--;
+            }
+            else if(!isalnum(s[i])){
+                s.erase(s.begin()+i);
+                i--;
+            }
+        }
+        for(int i = 0; i<s.length(); i++){
+            if(isupper(s[i])){
+                s[i] = tolower(s[i]);
+            }
+        }
+        
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s[left]!=s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
