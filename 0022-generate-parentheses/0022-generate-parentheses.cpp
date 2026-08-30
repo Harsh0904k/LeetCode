1a590a9af7b1d53103249402108dff1b38663260
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-08-30 14:47:32 +0530

    Time: 2 ms (80.24%), Space: 13 MB (89.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -3,7 +3,6 @@ public:
     void check(int open, int close, string& strng, int n, vector<string>& ans){
         if(strng.length()==2*n){
             ans.push_back(strng);
-            return;
         }
         if(open<n){
             strng.push_back('(');
