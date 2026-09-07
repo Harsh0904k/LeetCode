4b3aca33c4b04a3323c73aca8aa0ab7d9701822b
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-09-07 13:10:53 +0530

    Time: 76 ms (37.22%), Space: 113.5 MB (16.08%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i = 0; i<nums.size()-1; i++){
-            if(nums[i]==nums[i+1]){
+        unordered_map<int,int> freq;
+        for(int i = 0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        for(auto x:freq){
+            if(x.second>1){
                 return true;
             }
         }
