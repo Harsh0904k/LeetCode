76c79690a04561f7ff6f76ee461f8dc73ca15bdd
Harsh Kumar <160148212+Harsh0904k@users.noreply.github.com>
2026-09-07 12:29:57 +0530

    Time: 1 ms (77.07%), Space: 14.9 MB (28.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,7 +6,6 @@ public:
     
         for(int i = 0; i<nums.size();i++){
             if(map.contains(target - nums[i])){
-                cout<<"ran1";
                 ans.push_back(i);
                 for(auto x:map){
                     if(x.first == target - nums[i]){
@@ -15,7 +14,6 @@ public:
                     }
                 }
             }
-            cout<<"ran2";
             map[nums[i]] = i;
         }
         return ans;
